22373d1f740a44519115b704b8465884d4258464
Choconuts <35882095+Choconuts@users.noreply.github.com>
2019-09-29 19:22:13 +0800

    first

---
+++ .env.py
@@ -0,0 +1,12 @@
+
+class Config:
+    pass
+
+# path
+
+
+paths = Config()
+paths.project = Config()
+paths.project.base_dir = r'E:\PycharmProjects\KenBurns3D'
+
+# parameter
---
+++ unit_test/config/test_env_conf.py
@@ -0,0 +1,6 @@
+__import__('env.py')
+__import__('unit_test.cloud')
+
+
+if __name__ == '__main__':
+    pass
